b2d63616acc1ef49bbddcff3176567df3c55e456
Mag <hicugi@gmail.com>
2023-09-06 12:26:54 +0600

    fix: sort chapters & images

--- js/app.js
+++ js/app.js
@@ -11,35 +11,53 @@ document.querySelector("#btnSelectDir").addEventListener("click", async () => {
     const options = { mode: "read" };
     const dirHandle = await window.showDirectoryPicker(options);
 
-    chaptersElm.innerHTML = "";
-
     for await (const [key, value] of dirHandle.entries()) {
       if (value.kind !== "directory") continue;
-
-      const option = document.createElement("option");
-      option.value = key;
-      option.innerText = value.name;
-
       chapters[key] = value;
-
-      chaptersElm.appendChild(option);
     }
   } catch (err) {
     document.querySelector("#alert").innerText = err.message;
   }
+
+  chaptersElm.innerHTML = "<option selected>Select a chapter</option>";
+
+  const format = (s) => Number(s.replace("-", "."));
+  Object.keys(chapters)
+    .sort((a, b) => format(a) - format(b))
+    .forEach((key) => {
+      const value = chapters[key];
+      const option = document.createElement("option");
+
+      option.value = key;
+      option.innerText = value.name;
+
+      chaptersElm.appendChild(option);
+    });
 });
 
 chaptersElm.addEventListener("change", async (e) => {
   const dirHandle = chapters[e.target.value];
+  const images = {};
 
-  for await (const [, value] of dirHandle.entries()) {
-    if (value.kind !== "file") continue;
-
-    const img = document.createElement("img");
-    img.src = URL.createObjectURL(await value.getFile());
-
-    imagesElm.appendChild(img);
+  for await (const [key, value] of dirHandle.entries()) {
+    if (value.kind === "file" && key.match(/.*\.(jpg|jpeg|png|gif)$/i)) {
+      images[key] = value;
+    }
   }
+
+  imagesElm.innerText = "";
+
+  const format = (s) => Number(s.substring(0, s.indexOf(".")));
+  Object.keys(images)
+    .sort((a, b) => format(a) - format(b))
+    .forEach(async (key) => {
+      const value = images[key];
+      const img = document.createElement("img");
+
+      img.src = URL.createObjectURL(await value.getFile());
+
+      imagesElm.appendChild(img);
+    });
 });
 
 document.querySelector("#scrollToTop").addEventListener("click", () => {
