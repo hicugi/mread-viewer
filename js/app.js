1971595f7b81dcb09a8bc7af272d79ae6e893c51
Mag <hicugi@gmail.com>
2023-09-06 13:55:57 +0600

    feat: navigation arrows

--- js/app.js
+++ js/app.js
@@ -4,39 +4,9 @@ const alertElm = document.querySelector("#alert");
 
 const chapters = {};
 
-document.querySelector("#btnSelectDir").addEventListener("click", async () => {
-  alertElm.innerText = "";
-
-  try {
-    const options = { mode: "read" };
-    const dirHandle = await window.showDirectoryPicker(options);
-
-    for await (const [key, value] of dirHandle.entries()) {
-      if (value.kind !== "directory") continue;
-      chapters[key] = value;
-    }
-  } catch (err) {
-    document.querySelector("#alert").innerText = err.message;
-  }
-
-  chaptersElm.innerHTML = "<option selected>Select a chapter</option>";
-
-  const format = (s) => Number(s.replace("-", "."));
-  Object.keys(chapters)
-    .sort((a, b) => format(a) - format(b))
-    .forEach((key) => {
-      const value = chapters[key];
-      const option = document.createElement("option");
-
-      option.value = key;
-      option.innerText = value.name;
-
-      chaptersElm.appendChild(option);
-    });
-});
-
-chaptersElm.addEventListener("change", async (e) => {
-  const dirHandle = chapters[e.target.value];
+const selectChapter = async (chapterKey) => {
+  console.log(chapterKey);
+  const dirHandle = chapters[chapterKey];
   const images = {};
 
   for await (const [key, value] of dirHandle.entries()) {
@@ -58,8 +28,63 @@ chaptersElm.addEventListener("change", async (e) => {
 
       imagesElm.appendChild(img);
     });
+};
+
+document.querySelector("#btnSelectDir").addEventListener("click", async () => {
+  alertElm.innerText = "";
+
+  try {
+    const options = { mode: "read" };
+    const dirHandle = await window.showDirectoryPicker(options);
+
+    for await (const [key, value] of dirHandle.entries()) {
+      if (value.kind !== "directory") continue;
+      chapters[key] = value;
+    }
+  } catch (err) {
+    document.querySelector("#alert").innerText = err.message;
+  }
+
+  chaptersElm.innerHTML = "<option selected disabled>Select a chapter</option>";
+
+  const format = (s) => Number(s.replace("-", "."));
+  Object.keys(chapters)
+    .sort((a, b) => format(a) - format(b))
+    .forEach((key) => {
+      const value = chapters[key];
+      const option = document.createElement("option");
+
+      option.value = key;
+      option.innerText = value.name;
+
+      chaptersElm.appendChild(option);
+    });
+});
+
+chaptersElm.addEventListener("change", (e) => {
+  selectChapter(e.target.value);
 });
 
 document.querySelector("#scrollToTop").addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+const getSelectedOptionIndex = () => chaptersElm.options.selectedIndex;
+document.querySelector("#navPrev").addEventListener("click", () => {
+  const index = getSelectedOptionIndex();
+  const prevChapter = chaptersElm.options[index - 1];
+
+  if (!chapters[prevChapter.value]) return;
+
+  chaptersElm.value = prevChapter.value;
+  selectChapter(prevChapter.value);
+});
+document.querySelector("#navNext").addEventListener("click", () => {
+  const index = getSelectedOptionIndex();
+  const nextChapter = chaptersElm.options[index + 1];
+
+  if (!chapters[nextChapter.value]) return;
+
+  chaptersElm.value = nextChapter.value;
+  selectChapter(nextChapter.value);
 });
